acba625a2f2aedc9c3118db9eb77719e9ddf3cf6
fedelway <fede21perez@hotmail.com>
2016-03-28 20:13:26 -0300

    Me tengo que ir

--- src/test/java/ventas/Negocio.java
+++ src/test/java/ventas/Negocio.java
@@ -1,4 +1,5 @@
 package ventas;
+import java.time.*;
 import java.util.*;
 
 public class Negocio {
@@ -6,7 +7,9 @@ public class Negocio {
 	private Collection<Venta> ventas;
 	
 	public double precioFinal(Articulo articulo){
-		
+		/*
+		 * Obtiene el precio final de un articulo de acuerdo al negocio
+		 */
 		if (articulo.esImportado()){
 			return ( this.valorFijo + articulo.getPrecio() ) * (articulo.getTasa() + 1);
 		}
@@ -15,13 +18,23 @@ public class Negocio {
 	}
 	
 	public void vender(Articulo articulo, int cant){
+		/*
+		 * crea el objeto venta, le asigna sus atributos y lo añade a la lista de ventas
+		 */
 		Venta venta = new Venta();
 		venta.setArticulo(articulo);
-		venta.setCant(cant);
-		venta.setFecha()
-		
-		
+		venta.setCantidad(cant);
+		venta.setFecha(LocalDateTime.now());
+		venta.setPrecioFinal(this.precioFinal(articulo) );
 		ventas.add(venta);
 	}
 	
+	public double gananciaDelDia(LocalDateTime dia){
+		Collection<Venta> ventasDelDia;
+		
+		for(Venta v : ventas){
+			if
+		}
+		
+		}
 }
--- src/test/java/ventas/Venta.java
+++ src/test/java/ventas/Venta.java
@@ -1,9 +1,25 @@
 package ventas;
-import java.util.*;
+import java.time.*;;
 
 public class Venta{
 	private Articulo articulo;
 	private int cantidad;
-	private Date fecha;
+	private double precioFinal;
+	private LocalDateTime fecha;
 	
+	public void setFecha(LocalDateTime f){
+		fecha = f;
+	}
+	
+	public void setCantidad(int cant){
+		cantidad = cant;
+	}
+	
+	public void setArticulo (Articulo art){
+		articulo = art;
+	}
+	
+	public void setPrecioFinal(double precio){
+		precioFinal = precio;
+	}
 }
